3d47cb39507e7289521f7658ee32ed35b5d43372
Your Name <bf-formula@hs-offenburg.de>
2021-03-16 17:29:25 +0100

    AD0034 - Adjust naming for CAN msgs to ROS topic package and files

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['bfft_CAN_msgs_to_ROS_topic', 'bfft_CAN_msgs_to_ROS_topic.utilities_IMU'],
+     packages=['bfft_can_bus_msgs_to_ros_topic', 'bfft_can_bus_msgs_to_ros_topic.utilities_IMU'],
      package_dir={'': 'src'}
 )
 
