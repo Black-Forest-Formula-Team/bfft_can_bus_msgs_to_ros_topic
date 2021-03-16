c477d990ea220fc89b8146c13e401fab47f1f8b8
Your Name <bf-formula@hs-offenburg.de>
2021-03-16 17:20:31 +0100

    AD0034 - Adjust naming for CAN msgs to ROS topic package and files

--- src/bfft_CAN_msgs_to_ROS_topic/utilities_IMU/utility_classes.py
+++ src/bfft_CAN_msgs_to_ROS_topic/utilities_IMU/utility_classes.py
@@ -4,8 +4,8 @@
 import rospy
 from std_msgs.msg import String
 
-from bfft_CAN_msgs_to_ROS_topic.msg import Adma_status_errors_msg
-from bfft_CAN_msgs_to_ROS_topic.msg import Can_id_msg
+from bfft_CAN_msgs_to_ROS_topic.msg import AdmaStatusErrorsMsgs
+from bfft_CAN_msgs_to_ROS_topic.msg import CanIdMsgs
 
 class StatusErrorClass():
     """Status, Error and Warning class for holding information until puplished to topic
@@ -35,44 +35,44 @@ class StatusErrorClass():
     def __init__(self):
         super(StatusErrorClass, self).__init__()
         
-        self.__adma_status_errors_msg = Adma_status_errors_msg()
+        self.__AdmaStatusErrorsMsg = AdmaStatusErrorsMsgs()
     
     def set_status(self, status_msgs):
-        self.__adma_status_errors_msg.Stat_Byte0_GPS_Mode=status_msgs['Stat_Byte0_GPS_Mode']
-        self.__adma_status_errors_msg.Stat_Byte0_Standstill=status_msgs['Stat_Byte0_Standstill']
-        self.__adma_status_errors_msg.Stat_Byte0_Skidding=status_msgs['Stat_Byte0_Skidding']
-        self.__adma_status_errors_msg.Stat_Byte0_External_Vel_Out=status_msgs['Stat_Byte0_External_Vel_Out']
-        self.__adma_status_errors_msg.Stat_Byte1_Trig_GPS=status_msgs['Stat_Byte1_Trig_GPS']
-        self.__adma_status_errors_msg.Stat_Byte1_Signal_IN3=status_msgs['Stat_Byte1_Signal_IN3']
-        self.__adma_status_errors_msg.Stat_Byte1_Signal_IN2=status_msgs['Stat_Byte1_Signal_IN2']
-        self.__adma_status_errors_msg.Stat_Byte1_Signal_IN1=status_msgs['Stat_Byte1_Signal_IN1']
-        self.__adma_status_errors_msg.Stat_Byte1_Alignment=status_msgs['Stat_Byte1_Alignment']
-        self.__adma_status_errors_msg.Stat_Byte1_AHRS_INS=status_msgs['Stat_Byte1_AHRS_INS']
-        self.__adma_status_errors_msg.Stat_Byte1_Deadreckoning=status_msgs['Stat_Byte1_Deadreckoning']
-        self.__adma_status_errors_msg.Stat_Byte1_SyncLock=status_msgs['Stat_Byte1_SyncLock']
-        self.__adma_status_errors_msg.Stat_Byte2_EVK_activ=status_msgs['Stat_Byte2_EVK_activ']
-        self.__adma_status_errors_msg.Stat_Byte2_EVK_Estimates=status_msgs['Stat_Byte2_EVK_Estimates']
-        self.__adma_status_errors_msg.Stat_Byte2_Tilt=status_msgs['Stat_Byte2_Tilt']
-        self.__adma_status_errors_msg.Stat_Byte2_Pos=status_msgs['Stat_Byte2_Pos']
+        self.__AdmaStatusErrorsMsg.Stat_Byte0_GPS_Mode=status_msgs['Stat_Byte0_GPS_Mode']
+        self.__AdmaStatusErrorsMsg.Stat_Byte0_Standstill=status_msgs['Stat_Byte0_Standstill']
+        self.__AdmaStatusErrorsMsg.Stat_Byte0_Skidding=status_msgs['Stat_Byte0_Skidding']
+        self.__AdmaStatusErrorsMsg.Stat_Byte0_External_Vel_Out=status_msgs['Stat_Byte0_External_Vel_Out']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_Trig_GPS=status_msgs['Stat_Byte1_Trig_GPS']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_Signal_IN3=status_msgs['Stat_Byte1_Signal_IN3']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_Signal_IN2=status_msgs['Stat_Byte1_Signal_IN2']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_Signal_IN1=status_msgs['Stat_Byte1_Signal_IN1']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_Alignment=status_msgs['Stat_Byte1_Alignment']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_AHRS_INS=status_msgs['Stat_Byte1_AHRS_INS']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_Deadreckoning=status_msgs['Stat_Byte1_Deadreckoning']
+        self.__AdmaStatusErrorsMsg.Stat_Byte1_SyncLock=status_msgs['Stat_Byte1_SyncLock']
+        self.__AdmaStatusErrorsMsg.Stat_Byte2_EVK_activ=status_msgs['Stat_Byte2_EVK_activ']
+        self.__AdmaStatusErrorsMsg.Stat_Byte2_EVK_Estimates=status_msgs['Stat_Byte2_EVK_Estimates']
+        self.__AdmaStatusErrorsMsg.Stat_Byte2_Tilt=status_msgs['Stat_Byte2_Tilt']
+        self.__AdmaStatusErrorsMsg.Stat_Byte2_Pos=status_msgs['Stat_Byte2_Pos']
         
         
     def set_errors(self, error_msgs):
-        self.__adma_status_errors_msg.Errors_Byte0_HW=error_msgs['Errors_Byte0_HW']
-        self.__adma_status_errors_msg.Errors_Byte0_Nibble1=error_msgs['Errors_Byte0_Nibble1']
-        self.__adma_status_errors_msg.Errors_Byte1_Nibble0=error_msgs['Errors_Byte1_Nibble0']
-        self.__adma_status_errors_msg.Errors_Byte1_Nibble1=error_msgs['Errors_Byte1_Nibble1']
-        self.__adma_status_errors_msg.Warn_Byte2_GPS=error_msgs['Warn_Byte2_GPS']
+        self.__AdmaStatusErrorsMsg.Errors_Byte0_HW=error_msgs['Errors_Byte0_HW']
+        self.__AdmaStatusErrorsMsg.Errors_Byte0_Nibble1=error_msgs['Errors_Byte0_Nibble1']
+        self.__AdmaStatusErrorsMsg.Errors_Byte1_Nibble0=error_msgs['Errors_Byte1_Nibble0']
+        self.__AdmaStatusErrorsMsg.Errors_Byte1_Nibble1=error_msgs['Errors_Byte1_Nibble1']
+        self.__AdmaStatusErrorsMsg.Warn_Byte2_GPS=error_msgs['Warn_Byte2_GPS']
 
     def publish_status_errors(self):
         ''' Bring errors and status data into msg format to be published on topic'''
-        return self.__adma_status_errors_msg
+        return self.__AdmaStatusErrorsMsg
 
 class UnknownCANIdsClass():
     """Unknown CAN Ids for data visualization"""
 
     def __init__(self):
         super(UnknownCANIdsClass, self).__init__()
-        self.__can_msg=Can_id_msg()
+        self.__CanMsg=CanIdMsgs()
 
     def set_types_values(self, can_id, can_msgs, header):
         '''        #Save values and type of CAN frame (0-3) in class array, currently limited to 4 values
@@ -81,29 +81,29 @@ class UnknownCANIdsClass():
         keys=list(can_msgs.keys())
         keys_length=len(keys)
         try:
-            self.__can_msg.can_id=can_id
+            self.__CanMsg.can_id=can_id
         
             #Save header from CAN frame
-            self.__can_msg.seq=header.seq
-            self.__can_msg.stamp.secs=header.stamp.secs
-            self.__can_msg.stamp.nsecs=header.stamp.nsecs
-            self.__can_msg.frame_id=header.frame_id
+            self.__CanMsg.seq=header.seq
+            self.__CanMsg.stamp.secs=header.stamp.secs
+            self.__CanMsg.stamp.nsecs=header.stamp.nsecs
+            self.__CanMsg.frame_id=header.frame_id
             
             if keys_length>1:
-                self.__can_msg.value_0=can_msgs[keys[0]]
-                self.__can_msg.type_col_0=keys[0]
+                self.__CanMsg.value_0=can_msgs[keys[0]]
+                self.__CanMsg.type_col_0=keys[0]
             
             if keys_length>2:
-                self.__can_msg.value_1=can_msgs[keys[1]]
-                self.__can_msg.type_col_1=keys[1]
+                self.__CanMsg.value_1=can_msgs[keys[1]]
+                self.__CanMsg.type_col_1=keys[1]
             
             if keys_length>3:
-                self.__can_msg.value_2=can_msgs[keys[2]]
-                self.__can_msg.type_col_2=keys[2]
+                self.__CanMsg.value_2=can_msgs[keys[2]]
+                self.__CanMsg.type_col_2=keys[2]
             
             if keys_length>4:
-                self.__can_msg.value_3=can_msgs[keys[3]]
-                self.__can_msg.type_col_3=keys[3]          
+                self.__CanMsg.value_3=can_msgs[keys[3]]
+                self.__CanMsg.type_col_3=keys[3]          
             
         except rospy.ROSInterruptException:
             pass
@@ -112,4 +112,4 @@ class UnknownCANIdsClass():
         '''Returning the custom message format as a template for all incoming unknown CAN IDs 
         to be published (for later data visualization purpose)'''
         
-        return self.__can_msg
+        return self.__CanMsg
